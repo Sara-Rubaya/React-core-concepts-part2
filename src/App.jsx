2d7fa47f0834062cbde615cb8049af1ccc80d4a2
Sara <sara@Mac.lan>
2025-03-28 13:09:58 +0600

    added new react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 
+import { Suspense } from 'react';
 import './App.css'
 import Batsman from './Batsman';
 import Counter from './counter';
+import Users from './users';
+import Friends from './Friends';
+import Posts from './posts';
+import Players from './Players';
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+
+
+// const fetchFriends = async () =>{
+// const res = await fetch('https://jsonplaceholder.typicode.com/users');
+// return res.json();
+// }
+
+// const fetchPosts = async() =>{
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   return res.json();
+// }
 
 
 function App() {
- 
+// const postsPromise = fetchPosts();
+
+//  const friendsPromise = fetchFriends();
+
   function handleClick(){
     alert('I am clicked')
   }
@@ -24,8 +46,23 @@ const handleAdd5 = (num) => {
     <>
      
       <h3>Vite + React</h3>
+
+{/* <Suspense fallback={<h4>Post are coming...</h4>}>
+<Posts postsPromise={postsPromise}></Posts>
+</Suspense> */}
+
+<Players></Players>
+
 <Batsman></Batsman>
 <Counter></Counter>
+
+{/* <Suspense fallback={<h3>Loading...</h3>}>
+  <Users fetchUsers = {fetchUsers}></Users>
+</Suspense> */}
+
+{/* <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+   <Friends friendsPromise ={friendsPromise}></Friends>
+</Suspense> */}
 
       <button onClick={handleClick}>Click Me</button>
       <button onClick={function handleClick2(){
